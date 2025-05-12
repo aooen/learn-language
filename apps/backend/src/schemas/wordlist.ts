2cb92cffce1df43e7e19d09e5c9456e21a7345e5
ukyeong-user <hyk040520@naver.com>
2025-05-12 20:03:01 +0900

    chore(schema) : redefine wordlist table

--- apps/backend/src/schemas/wordlist.ts
+++ apps/backend/src/schemas/wordlist.ts
@@ -3,8 +3,7 @@ import { users } from './users';
 
 export const wordlist = mysqlTable('wordlist', {
   id: int('id').primaryKey().autoincrement(),
-  word: varchar('word', { length: 255 }).notNull(),
-  meaning: varchar('meaning', { length: 255 }).notNull(),
+  title: varchar('title', { length: 255 }).notNull(),
   userId: int('user_id')
     .notNull()
     .references(() => users.id),
