fa2c90f85db367a7c07c419f8742c9a752666af8
lyle_Kim <kimjoon0605@gmail.com>
2025-05-14 16:45:45 +0900

    withMedia

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,6 +3,7 @@ import { cors } from 'hono/cors';
 
 import collect from './routes/collect';
 import user from './routes/user';
+import mediaInfo from './routes/mediaInfo.ts' 
 import type { Env } from './types/hono';
 
 import './startup.ts';
@@ -14,8 +15,8 @@ const app = new Hono<Env>()
     await next();
   })
   .route('/collect', collect)
-  .route('/user', user);
-
+  .route('/user', user)
+  .route('/mediaInfo',mediaInfo);
 export type AppType = typeof app;
 
 export default app;
