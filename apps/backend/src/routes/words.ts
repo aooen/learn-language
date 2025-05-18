9efbb466636caaf84fce5759cd3b5b383a356853
Hakyeon Kim <khy@aooen.com>
2025-05-19 01:41:31 +0900

    chore: handle schema/index removal

--- apps/backend/src/routes/words.ts
+++ apps/backend/src/routes/words.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono';
 import { z } from 'zod';
 import { zValidator } from '~/utils/validator-wrapper';
-import { word } from '~/schemas';
+import { word } from '~/schemas/word';
 import { db } from '~/utils/db';
 import { inArray } from 'drizzle-orm';
 import type { Env } from '~/types/hono';
